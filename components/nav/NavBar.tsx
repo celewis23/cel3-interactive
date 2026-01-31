48d272854ad44810c3d77b2327c789e1fa81e3c9
Clarence Lewis <cel3media@gmail.com>
2026-01-31 10:09:34 -0500

    fix to for the X

--- components/nav/NavBar.tsx
+++ components/nav/NavBar.tsx
@@ -111,7 +111,7 @@ export function NavBar() {
 
       {/* Mobile overlay menu */}
       {open ? (
-        <div className="md:hidden fixed inset-0 z-50">
+        <div className="md:hidden fixed inset-0 z-40">
           {/* Backdrop */}
           <button
             aria-label="Close menu"
